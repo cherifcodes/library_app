9bb70f66847965cbd99a84df42c8685751b67812
cherif.diallo <cherif.diallo@dealerware.com>
2024-12-21 05:50:10 -0500

    Build for release.

--- build/web/main.dart.js
+++ build/web/main.dart.js
@@ -7770,11 +7770,12 @@ B0:function B0(){},
 ae0(){return new A.p3(new A.Nt(),A.w(t.K,t.cP))},
 UG:function UG(a,b){this.a=a
 this.b=b},
-pB:function pB(a,b,c,d){var _=this
+pB:function pB(a,b,c,d,e){var _=this
 _.e=a
 _.cx=b
 _.db=c
-_.a=d},
+_.R8=d
+_.a=e},
 Nt:function Nt(){},
 PV:function PV(){},
 tv:function tv(){this.d=$
@@ -50438,7 +50439,7 @@ en(a,b){var s=this.a,r=J.bG(s)
 if(b?r.B(s,a):r.v(s,a))this.aC()}}
 A.yn.prototype={
 O(a){var s=null
-return new A.pB(B.Bl,"Flutter Demo",A.a2_(s,A.a3R(B.I,s,s,B.Bc),!0),s)}}
+return new A.pB(B.Bl,"Library manager",A.a2_(s,A.a3R(B.I,s,s,B.Bc),!0),!1,s)}}
 A.pP.prototype={
 ah(){return new A.tB()}}
 A.tB.prototype={
@@ -50446,7 +50447,7 @@ MM(){this.aD(new A.Y3(this))},
 O(a){var s=null,r=A.aG(a).ax,q=r.y2
 r=q==null?r.c:q
 this.a.toString
-q=A.a6y("Flutter Demo Home Page",s)
+q=A.a6y("Library manager",s)
 return new A.qK(new A.nE(q,r,new A.Eq(s,s,1/0,56),s),A.IK(A.acf(A.a([B.Jp,A.a6y(""+this.d,A.aG(a).p2.e)],t.nA),B.nG),s,s),A.adg(B.xL,!1,this.gML(),"Increment"),s)}}
 A.Y3.prototype={
 $0(){++this.a.d},
